2083550a1cc93d871b7eb1b07df5d25a21d10587
Jeff Powell <jeffrey@springboardmedia.com>
2017-03-16 10:58:13 -0400

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+ #Base Image built from most current Java version OFFICIAL
+FROM java:openjdk-9-b149-jre
+
+ #Install Prereqs
+RUN apt-get update \
+    && apt-get -qy --no-install-recommends install \
+        cpio \
+        curl \
+        sed \
+        wget \
+    && apt-get -q -y clean \
+    && rm -rf /var/cache/apt/archives/* /var/lib/apt/lists/* \
+    && rm -rf /usr/share/man/?? /usr/share/man/??_*
+
+ #Configuring ports and setting up Data Volumes
+EXPOSE 4282 4283 4285 4286
+VOLUME ["/var/log/springback","/var/opt/springback","/opt/springback"]
+
+ #Copying springback.sh install and start script
+ #Crashplan downlaods for the first time
+COPY ./springback.sh /
+ENTRYPOINT ["/springback.sh"]
+ 
---
+++ springback.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+VERSION="5.4.2"
+MD5HASH=7ff0e69111d9d42dfae54eed5c600fe8
+CMD=/opt/proserver/bin/proserver
+INSTALLDIR=/tmp/src
+
+if [ ! -f $CMD ]
+then
+    mkdir -p $INSTALLDIR
+    echo "Download Code42 Server (CrashPlanPROServer)"
+    curl -fSL -o /tmp/Code42server_Linux.tgz https://download.code42.com/installs/proserver/${VERSION}/Code42server_${VERSION}_Linux.tgz
+    echo "Unpacking Code42 Server (CrashPlanPROServer)"
+    echo "$MD5HASH  /tmp/Code42server_Linux.tgz" | md5sum -c - &&\
+    tar -xzf /tmp/Code42server_Linux.tgz -C $INSTALLDIR --strip-components 1 &&\
+    rm -f /tmp/Code42server_Linux.tgz
+    echo "Starting installation of CrashPlanPROServer v$VERSION!"
+    sed -i 's/INSTALL_UNATTENDED=0/INSTALL_UNATTENDED=1/g' ${INSTALLDIR}/install.sh
+    sed -i 's/AUTO_ACCEPT_EULA=0/AUTO_ACCEPT_EULA=1/g' ${INSTALLDIR}/install.sh
+    cd $INSTALLDIR && ./install.sh
+    rm -rf $INSTALLDIR
+fi
+
+echo "Starting Services"
+/etc/init.d/proserver start
+touch /var/log/proserver/app.log
+tail -f /var/log/proserver/app.log
